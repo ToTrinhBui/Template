c6136f7604aafa7f4429f5b7c0a60c251c765e5e
ToTrinhBui <totriinh@gmail.com>
2023-03-04 21:37:17 +0700

    add data

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,13 +1,23 @@
 import React from "react";
-
+import datalist from '../data';
 export default function Meme() {
+    let imageUrl
+    function getImage(){
+        const memeArray = datalist.data.memes
+        const randomNumber = Math.floor(Math.random() * memeArray.length)
+        imageUrl = memeArray[randomNumber].url
+        console.log(imageUrl)
+    }
+
     return (
         <div className="meme">
             <div className="meme-input">
-                <input type="text" placeholder="Top"/>
+                <input type="text" placeholder="Top" />
                 <input type="text" placeholder="Bottom" />
             </div>
-            <button className="background">Get a new meme image</button>
+            <button className="background" onClick={getImage}>
+                Get a new meme image
+            </button>
         </div>
     )
 }
